0d10dee64fbfe524b557dc1e715e5cf176a2b488
jassimbandtech <jassimbandtech@gmail.com>
2026-04-26 20:14:58 +0300

    add projects

---
+++ index.js
@@ -0,0 +1,98 @@
+
+
+const projects = [
+  {
+    id: 1,
+    num: "01",
+    featured: true,
+    title: "Clinic System - Booking & Management Web",
+    description:
+    `Built a complete Clinic Management System designed to streamline daily operations and enhance patient experience.
+The system covers the full workflow: appointment booking, reception, waiting queue, doctor consultation...
+    `,
+    images: [
+      "./img/medical.png",
+      "./img/medical2.png",
+    //   "/img/medical3.png",
+    ],
+    tags: ["React Js", "Laravel", "MySQL", "Admin Panel"],
+    color: "#FF6B35",
+  },
+  {
+    id: 2,
+    num: "02",
+    featured: false,
+    title: "AllWisam Express — Logistics Company Website",
+    description:
+      "Bilingual (Arabic & English) website for a logistics client. Built with React, TypeScript, and Material-UI for a smooth, interactive, and fully localized user interface.",
+    images: [
+      "./img/wisam.png",
+      "./img/wisam2.png",
+    ],
+    tags: [ "TypeScript", "MUI",  "Next.js"],
+    color: "#00D2FF",
+  },
+  {
+    id: 3,
+    num: "03",
+    featured: false,
+    title: "Victoria — Furniture Store App",
+    description:
+      "Complete e-commerce solution for furniture products. Features a full product catalog, order management system, admin sales dashboard, and inventory/stock control. Published on Google Play.",
+    images: [
+      './img/vict.png'
+    ],
+    tags: ["React Native", "Laravel", "Google Play", "E-Commerce"],
+    color: "#A855F7",
+  },
+  {
+    id: 4,
+    num: "04",
+    featured: false,
+    title: "Ai Agent Support  Landing Page && Admin Dashboard",
+    description:
+`
+
+
+A modern AI-powered support system featuring a high-converting landing page and a powerful admin dashboard.
+The platform enables automated customer interactions, smart responses, and seamless conversation management.
+Includes real-time analytics, ticket tracking, and performance insights to optimize support operations.
+Designed with a clean UI/UX, fast performance, and scalable architecture for growing businesses.
+
+`,
+    images: [
+      "./img/ai1.png",
+      "./img/ai2.png",
+    ],
+    tags: ["React.js", "REST API", "Laravel", "RTL", "Laravel Sdk Ai "],
+    color: "#10B981",
+  },
+  {
+    id: 5,
+    num: "05",
+    featured: false,
+    title: "Al-Andalus Car Insurance Dashboard",
+    description:
+      "Advanced insurance management system with role-based UI, comprehensive financial analytics, reporting tools, and a workflow optimized for insurance operations at scale.",
+    images: [
+  './img/andlus.png',
+    ],
+    tags: ["Next.js",  "MUI" , 'tailwind'],
+    color: "#F59E0B",
+  },
+  {
+    id: 6,
+    num: "06",
+    featured: false,
+    title: "Ai Agent Restaurant Website",
+    description:
+  `Elegant restaurant website with a premium dark UI, offering a smooth browsing and booking experience.
+It features an AI chatbot that helps users explore the menu, answer questions, and make table reservations  
+`,
+    images: [
+  './img/rest.png',
+    ],
+    tags: ["Inertia.js",  "MUI" , 'tailwind', 'Laravel Sdk Ai'],
+    color: "#f53a0b",
+  },
+];
